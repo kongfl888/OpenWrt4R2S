24347fe70887054fe21d9edfce010592c7a869b2
kongfl888 <kongfl888@outlook.com>
2020-06-28 01:35:44 +0800

    add wol accesscontrol and fix luci-lib-fs is miss.

--- scripts/original-package.sh
+++ scripts/original-package.sh
@@ -9,6 +9,10 @@ leanpack="friendlywrt-rk3328/friendlywrt/package/lean"
 
 mkdir -p $leanpack
 
+#get luci-app-accesscontrol
+rm -rf friendlywrt-rk3328/feeds/*/*/luci-app-accesscontrol/ >/dev/null 2>&1 || echo ""
+cp -rf openwrt/package/lean/luci-app-accesscontrol/ $leanpack
+
 # get adbyby
 rm -rf friendlywrt-rk3328/feeds/package/*/adbyby/ >/dev/null 2>&1 || echo ""
 rm -rf friendlywrt-rk3328/feeds/*/*/luci-app-adbyby-plus/ >/dev/null 2>&1 || echo ""
@@ -27,6 +31,10 @@ cp -rf openwrt/package/lean/luci-app-ramfree/ $leanpack
 rm -rf friendlywrt-rk3328/feeds/*/*/luci-app-vlmcsd/ >/dev/null 2>&1 || echo ""
 cp -rf openwrt/package/lean/vlmcsd/ $leanpack
 cp -rf openwrt/package/lean/luci-app-vlmcsd/ $leanpack
+
+#get luci-lib-fs
+rm -rf friendlywrt-rk3328/feeds/*/*/luci-lib-fs/ >/dev/null 2>&1 || echo ""
+cp -rf openwrt/package/lean/luci-lib-fs/ $leanpack
 
 #get luci-app-filetransfer
 rm -rf friendlywrt-rk3328/feeds/*/*/luci-app-filetransfer/ >/dev/null 2>&1 || echo ""

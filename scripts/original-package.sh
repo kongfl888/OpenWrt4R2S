98376e2d4eac497ee5991554a251ec0b5ef48ebd
kongfl888 <kongfl888@outlook.com>
2020-06-29 12:50:45 +0800

    software and dependence

--- scripts/original-package.sh
+++ scripts/original-package.sh
@@ -93,6 +93,43 @@ rm -rf friendlywrt-rk3328/friendlywrt/feeds/*/*/rblibtorrent/ >/dev/null 2>&1 ||
 cp -rf openwrt/package/lean/rblibtorrent/ $leanpack
 rm -rf friendlywrt-rk3328/friendlywrt/feeds/*/*/luci-app-qbittorrent/ >/dev/null 2>&1 || echo ""
 cp -rf openwrt/package/lean/luci-app-qbittorrent/ $leanpack
+
+# get unblockmusic
+rm -rf friendlywrt-rk3328/friendlywrt/feeds/*/*/luci-app-unblockmusic/ >/dev/null 2>&1 || echo ""
+cp -rf openwrt/package/lean/luci-app-unblockmusic/ $leanpack
+rm -rf friendlywrt-rk3328/friendlywrt/feeds/*/*/UnblockNeteaseMusic-Go/ >/dev/null 2>&1 || echo ""
+cp -rf openwrt/package/lean/UnblockNeteaseMusic-Go/ $leanpack
+rm -rf friendlywrt-rk3328/friendlywrt/feeds/*/*/UnblockNeteaseMusic/ >/dev/null 2>&1 || echo ""
+cp -rf openwrt/package/lean/UnblockNeteaseMusic/ $leanpack
+
+# get luci-app-flowoffload
+rm -rf friendlywrt-rk3328/friendlywrt/feeds/*/*/luci-app-flowoffload/ >/dev/null 2>&1 || echo ""
+cp -rf openwrt/package/lean/luci-app-flowoffload/ $leanpack
+
+# get luci-app-ssrserver-python
+rm -rf friendlywrt-rk3328/friendlywrt/feeds/*/*/luci-app-ssrserver-python/ >/dev/null 2>&1 || echo ""
+cp -rf openwrt/package/lean/luci-app-ssrserver-python/ $leanpack
+
+#get ssr
+rm -rf friendlywrt-rk3328/friendlywrt/feeds/*/*/shadowsocksr-libev/ >/dev/null 2>&1 || echo ""
+cp -rf openwrt/package/lean/shadowsocksr-libev/ $leanpack
+rm -rf friendlywrt-rk3328/friendlywrt/feeds/*/*/kcptun/ >/dev/null 2>&1 || echo ""
+cp -rf openwrt/package/lean/kcptun/ $leanpack
+rm -rf friendlywrt-rk3328/friendlywrt/feeds/*/*/srelay/ >/dev/null 2>&1 || echo ""
+cp -rf openwrt/package/lean/srelay/ $leanpack
+rm -rf friendlywrt-rk3328/friendlywrt/feeds/*/*/microsocks/ >/dev/null 2>&1 || echo ""
+cp -rf openwrt/package/lean/microsocks/ $leanpack
+rm -rf friendlywrt-rk3328/friendlywrt/feeds/*/*/proxychains-ng/ >/dev/null 2>&1 || echo ""
+cp -rf openwrt/package/lean/proxychains-ng/ $leanpack
+rm -rf friendlywrt-rk3328/friendlywrt/feeds/*/*/shadowsocks-libev/ >/dev/null 2>&1 || echo ""
+cp -rf openwrt/package/lean/shadowsocks-libev/ $leanpack
+rm -rf friendlywrt-rk3328/friendlywrt/feeds/*/*/tcpping/ >/dev/null 2>&1 || echo ""
+cp -rf openwrt/package/lean/tcpping/ $leanpack
+git clone -b master --single-branch https://github.com/aa65535/openwrt-simple-obfs  $leanpack/simple-obfs
+git clone -b master --single-branch https://github.com/kongfl888/redsocks2.git $leanpack/redsocks2
+#get luci-app-vssr
+git clone -b master --single-branch https://github.com/jerrykuku/lua-maxminddb.git  $leanpack/lua-maxminddb
+git clone -b master --single-branch https://github.com/jerrykuku/luci-app-vssr.git  $leanpack/luci-app-vssr
 # big
 fi
 

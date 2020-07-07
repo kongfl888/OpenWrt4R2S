194930ae14ef75beb9d33959fcba89441bcd0047
kongfl888 <kongfl888@outlook.com>
2020-07-07 21:53:20 +0800

    Kernel: remove needless patch.

--- scripts/patch_for_r2s_5.4.sh
+++ scripts/patch_for_r2s_5.4.sh
@@ -1,6 +1,9 @@
 cd friendlywrt-rk3328
 git clone https://github.com/openwrt/openwrt && cd openwrt/
-rm -f target/linux/generic/*led*.patch
+rm -f target/linux/generic/*/*led*.patch
+rm -f target/linux/generic/*/*mips*.patch
+rm -f target/linux/generic/*/*MIPS*.patch
+rm -f target/linux/generic/*/*x86*.patch
 cp -a ./target/linux/generic/files/* ../kernel/
 ./scripts/patch-kernel.sh ../kernel target/linux/generic/backport-5.4
 ./scripts/patch-kernel.sh ../kernel target/linux/generic/pending-5.4

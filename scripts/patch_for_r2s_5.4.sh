60dee9af99da573e4963e3fee177287358ed94cb
kongfl888 K <kongfl888@outlook.com>
2020-07-20 00:53:09 +0800

    update patch r2s 5.4

--- scripts/patch_for_r2s_5.4.sh
+++ scripts/patch_for_r2s_5.4.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
+#
+# [K] (c)2020
+#
+
+snapshot=0
+
+if [ "${1}" == "1" ]; then
+    snapshot=1
+fi
+
 cd friendlywrt-rk3328
-git clone https://github.com/openwrt/openwrt && cd openwrt/
+
+if [ "$snapshot" == "1" ]; then
+    cd friendlywrt
+else
+    git clone https://github.com/openwrt/openwrt && cd openwrt/
+fi
+
 rm -f target/linux/generic/*/*led*.patch
 rm -f target/linux/generic/*/*mips*.patch
 rm -f target/linux/generic/*/*MIPS*.patch
@@ -9,3 +26,7 @@ cp -a ./target/linux/generic/files/* ../kernel/
 ./scripts/patch-kernel.sh ../kernel target/linux/generic/pending-5.4
 ./scripts/patch-kernel.sh ../kernel target/linux/generic/hack-5.4
 cd ../
+
+if [ "$snapshot" != "1" ]; then
+    rm -rf openwrt || echo ""
+fi

8570de1ea3ede452f5ef2c11e32902fd0f21f8f1
kongfl888 K <kongfl888@outlook.com>
2020-07-26 12:29:27 +0800

    fix copyconfig

--- scripts/copyconfig.sh
+++ scripts/copyconfig.sh
@@ -6,12 +6,12 @@
 mkdir -p ./r2sconfig/
 
 cp friendlywrt-rk3328/friendlywrt/.config ./r2sconfig/wrt.config
-friendlywrt-rk3328/friendlywrt/scripts/diffconfig.sh > ./r2sconfig/wrt-user.config
+friendlywrt-rk3328/friendlywrt/scripts/diffconfig.sh > ./r2sconfig/wrt-user.config || echo ""
 cd friendlywrt-rk3328/kernel
 export PATH=/opt/FriendlyARM/toolchain/6.4-aarch64/bin/:$PATH
 export CROSS_COMPILE='aarch64-linux-gnu-'
 export ARCH=arm64
-make savedefconfig
+make savedefconfig || echo ""
 cd ../../
-cp friendlywrt-rk3328/kernel/.config ./r2sconfig/defconfig.config
-cp friendlywrt-rk3328/kernel/defconfig ./r2sconfig/defconfig-user.config
+cp friendlywrt-rk3328/kernel/.config ./r2sconfig/defconfig.config || echo ""
+cp friendlywrt-rk3328/kernel/defconfig ./r2sconfig/defconfig-user.config || echo ""

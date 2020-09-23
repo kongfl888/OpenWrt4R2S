0f857a95aaf12071172c2004c6b958d21ff20ef6
kongfl888 K <kongfl888@outlook.com>
2020-09-24 05:11:41 +0800

    Revert "snapshot: revert 1.6ghz."
    This reverts commit 1f4f8dd8d58eee5cbf6d5a97dd9b22da25ca2001.

--- scripts/patch_for_r2s.sh
+++ scripts/patch_for_r2s.sh
@@ -29,14 +29,14 @@ cd friendlywrt-rk3328
 cd kernel/
 
 # cpu overclocking
-#if [ "$profile" == "4" ]; then
-#    # enable-1.4/1.5/1.6ghz-opp
-#    git apply --check ../../patches/rk3328-enable-1.4-1.5-1.6ghz-opp.patch && git apply ../../patches/rk3328-enable-1.4-1.5-1.6ghz-opp.patch
-#else
+if [ "$profile" == "4" ]; then
+    # enable-1.4/1.5/1.6ghz-opp
+    git apply --check ../../patches/rk3328-enable-1.4-1.5-1.6ghz-opp.patch && git apply ../../patches/rk3328-enable-1.4-1.5-1.6ghz-opp.patch
+else
     # enable-1512mhz-opp
     wget https://github.com/armbian/build/raw/master/patch/kernel/rockchip64-dev/RK3328-enable-1512mhz-opp.patch
     git apply --check RK3328-enable-1512mhz-opp.patch && git apply RK3328-enable-1512mhz-opp.patch || echo ""
-#fi
+fi
 
 # fix when during system boot,GPIO ocnflict with sdmmc
 
